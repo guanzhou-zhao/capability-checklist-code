412ed3e41a14af85213b0632d0c271d2c532b242
Wen chi <wenchi@WendeMacBook-Pro.local>
2016-09-03 12:33:27 +1200

    Implement filter() and reduce()

--- functional-javascript.js
+++ functional-javascript.js
@@ -14,7 +14,66 @@ function find(arr, callback) {
   }
   return isFound ? result : undefined
 }
+function filter(arr, callback) {
+  var result = []
+  arr.forEach(function(item) {
+    if (callback(item)) {
+      result.push(item)
+    }
+  })
+  return result
+}
+function reduce(arr, callback, initValue) {
+  var result
+  var i = 0
+  if (typeof initValue !== "undefined") {
+    result = initValue
+  } else {
+    if (arr.length) {
+      result = arr[0]
+      i++
+    } else {
+      throw new Error("Reduce of empty array without inital value")
+    }
+  }
+
+  for(; i < arr.length; i++) {
+    result = callback(result, arr[i], i)
+  }
+
+  return result
+}
+function any(arr) {
+
+}
+function sum(arr) {
+
+}
+function product() {
+
+}
+function maximum() {
+
+}
+function minimum() {
+
+}
+function concat() {
+
+}
+function merge() {
+
+}
 module.exports = {
   map: map,
-  find: find
+  find: find,
+  filter: filter,
+  reduce: reduce,
+  any: any,
+  sum: sum,
+  product: product,
+  maximum: maximum,
+  minimum: minimum,
+  concat: concat,
+  merge: merge
 }
--- test/functional-javascript-test.js
+++ test/functional-javascript-test.js
@@ -32,3 +32,45 @@ test("test find()", function(t) {
   t.equal(actual2, expected2, "find() => array.find()  return undefined")
   t.end()
 })
+test("test filter()", function(t) {
+  var callbackReturnNonempty = function(item) {
+    return item > 2
+  }
+  var expected = numberArray.filter(callbackReturnNonempty)
+  var actual = arrayUtils.filter(numberArray, callbackReturnNonempty)
+  t.deepEqual(actual, expected, "filter() => array.filter()  return non empty array")
+
+  var callbackReturnEmpty = function(item) {
+    return item < 0
+  }
+  var expected2 = numberArray.filter(callbackReturnEmpty)
+  var actual2 = arrayUtils.filter(numberArray, callbackReturnEmpty)
+  t.deepEqual(actual2, expected2, "filter() => array.filter()  return empty array")
+  t.end()
+})
+test("test reduce()", function(t) {
+  var callbackReturnSum = function(preV, curV) {
+    return preV + curV
+  }
+  var expected = numberArray.reduce(callbackReturnSum, 0)
+  var actual = arrayUtils.reduce(numberArray, callbackReturnSum, 0)
+  t.deepEqual(actual, expected, "reduce() => array.reduce()  with inital value passed in")
+
+  var callbackReturnSum = function(preV, curV) {
+    return preV + curV
+  }
+  var expected2 = numberArray.reduce(callbackReturnSum)
+  var actual2 = arrayUtils.reduce(numberArray, callbackReturnSum)
+  t.deepEqual(actual2, expected2, "reduce() => array.reduce()  without inital value passed in")
+
+  var callbackReturnObject = function(preV, curV, index) {
+    preV[index] = curV
+    return preV
+  }
+  var expected2 = numberArray.reduce(callbackReturnObject, {})
+  var actual2 = arrayUtils.reduce(numberArray, callbackReturnObject, {})
+  console.log(expected2, actual2)
+  t.deepEqual(actual2, expected2, "reduce() => array.reduce()  reduce array to object")
+
+  t.end()
+})
